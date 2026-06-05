2ffe8cef7bc53b29fb64c15445572c57d132ae42
Sofiagiraldo01 <Sgiraldo_34@cue.edu.co>
2026-06-05 18:46:57 -0500

    cucumbertest

--- src/test/java/com/recargaya/bdd/CucumberTestRunner.java
+++ src/test/java/com/recargaya/bdd/CucumberTestRunner.java
@@ -1,4 +1,15 @@
 package com.recargaya.bdd;
 
+import io.cucumber.junit.platform.engine.Constants;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@IncludeEngines("cucumber")
+@ConfigurationParameter(
+        key = Constants.GLUE_PROPERTY_NAME,
+        value = "com.recargaya.bdd"
+)
 public class CucumberTestRunner {
 }
