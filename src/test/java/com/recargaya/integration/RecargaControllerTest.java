0740406a4436e3f6ff7aee4083aff493ef248a67
Sofiagiraldo01 <Sgiraldo_34@cue.edu.co>
2026-06-05 18:48:28 -0500

    recargacontrollertest

--- src/test/java/com/recargaya/integration/RecargaControllerTest.java
+++ src/test/java/com/recargaya/integration/RecargaControllerTest.java
@@ -1,4 +1,21 @@
 package com.recargaya.integration;
 
+import org.junit.jupiter.api.Test;
+
 public class RecargaControllerTest {
+    @Test
+    void debeRetornar200() {
+    }
+    @Test
+    void debeRetornarBonificacion10() {
+    }
+    @Test
+    void debeRetornarBonificacion25() {
+    }
+    @Test
+    void debeRetornarBonificacionPremium() {
+    }
+    @Test
+    void debeRetornar400MontoInvalido() {
+    }
 }
