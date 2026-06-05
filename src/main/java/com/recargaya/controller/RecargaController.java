9051caf4e6dbcd42cca434c3cb673adf371dd635
Sofiagiraldo01 <Sgiraldo_34@cue.edu.co>
2026-06-05 18:36:22 -0500

    recarga controller

--- src/main/java/com/recargaya/controller/RecargaController.java
+++ src/main/java/com/recargaya/controller/RecargaController.java
@@ -1,4 +1,27 @@
 package com.recargaya.controller;
 
+import com.recargaya.model.RecargaRequest;
+import com.recargaya.model.ResultadoRecarga;
+import com.recargaya.service.RecargaService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/recargas")
+@RequiredArgsConstructor
 public class RecargaController {
+    private final RecargaService service;
+
+    @PostMapping
+    public ResultadoRecarga calcular(
+            @RequestBody RecargaRequest request) {
+
+        return service.calcularRecarga(
+                request.monto(),
+                request.premium()
+        );
+    }
 }
