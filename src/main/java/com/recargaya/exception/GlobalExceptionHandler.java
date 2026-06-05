0a4d6577bdac9b737c32497cba8f741b71f6de7b
Sofiagiraldo01 <Sgiraldo_34@cue.edu.co>
2026-06-05 18:38:04 -0500

    globalexceptionhandler

--- src/main/java/com/recargaya/exception/GlobalExceptionHandler.java
+++ src/main/java/com/recargaya/exception/GlobalExceptionHandler.java
@@ -1,4 +1,23 @@
 package com.recargaya.exception;
 
-public class GlobalExceptionHandler {
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.Map;
+
+public class GlobalExceptionHandler{
+@ExceptionHandler(MontoInvalidoException.class)
+public ResponseEntity<Map<String,String>>
+manejarMontoInvalido(MontoInvalidoException ex){
+
+    return ResponseEntity
+            .status(HttpStatus.BAD_REQUEST)
+            .body(
+                    Map.of(
+                            "error",
+                            ex.getMessage()
+                    )
+            );
+}
 }
