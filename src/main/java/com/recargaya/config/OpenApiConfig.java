b0898e36bf7d5e8ac2d2188a891f123b40ad8b90
Sofiagiraldo01 <Sgiraldo_34@cue.edu.co>
2026-06-05 18:41:15 -0500

    openapiconfig

--- src/main/java/com/recargaya/config/OpenApiConfig.java
+++ src/main/java/com/recargaya/config/OpenApiConfig.java
@@ -1,4 +1,22 @@
 package com.recargaya.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
 public class OpenApiConfig {
+    @Bean
+    public OpenAPI api() {
+
+        return new OpenAPI()
+                .info(
+                        new Info()
+                                .title("RecargaYa API")
+                                .version("1.0")
+                );
+    }
+}
 }
